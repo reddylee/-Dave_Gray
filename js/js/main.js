4ad3de22845f8217e9eb7ad1123d8dce265aeba1
Reddy <reddylee@gmail.com>
2022-04-19 23:59:50 +0100

    5

---
+++ js/js/main.js
@@ -0,0 +1,7 @@
+console.log(Math.floor( Math.random()*10)+1);
+
+const anyName="Peppa"
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)))
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)))
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)))
+console.log(anyName.charAt(Math.floor(Math.random()*anyName.length)))
